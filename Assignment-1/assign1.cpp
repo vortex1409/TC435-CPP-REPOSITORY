baf264ee0f4fce11bd4fde97659a1415d04c44a6
Michael <mvd1409@gmail.com>
2015-10-26 10:15:55 -0400

    Create assign1.cpp

---
+++ Assignment-1/assign1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	printf("Assignment #1\nDeveloped by Michael Dorfman");
+	printf("\n");
+	printf("________________________________________________");
+	printf("\n");
+	printf("Step 1: After a reset occurs, the red light turns on.");
+	printf("\n");
+	system("pause");
+	printf("Step 2: Loop Starts");
+	printf("\n");
+	system("pause");
+	printf("Step 3: After 5 seconds, this light turns from red to green.");
+	printf("\n");
+	system("pause");
+	printf("Step 4: After 45 seconds or 30 cars have passed, the green light turns from green to yellow / amber.");
+	printf("\n");
+	system("pause");
+	printf("Step 5: After 5 seconds, the yellow / amber lights turns off and the red light turns on again.");
+
+// SPACER ---------------------------------------------------------
+	printf("\n");
+	printf("________________________________________________");
+	printf("\n");
+	system("pause");
+	return 0;
+}
