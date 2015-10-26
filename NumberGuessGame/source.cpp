dfafba388ac3033cf75f95106f363e22d5b2824f
Michael <mvd1409@gmail.com>
2015-10-26 10:44:59 -0400

    Create source.cpp

---
+++ NumberGuessGame/source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+	int input = 0;
+
+
+	cout << "Enter a number between 1-10: ";
+	cin >> input;
+
+	int guess = 0;
+	srand(time(NULL));
+	int random = rand() % 111;
+
+	for (guess = 2; guess >= 0; guess--) {
+
+		if (input == random) {
+			cout << "You have guessed correctly \n";
+			break;
+		}
+		else{
+			cout << "Guess Again \n" << "Enter another number: ";
+			cin >> input;
+		}
+
+	}
+
+	cout << "The random number is: " << random;
+
+
+
+	// SPACER ---------------------------------------------------------
+	printf("\n");
+	printf("________________________________________________");
+	printf("\n");
+	system("pause");
+	return 0;
+}
