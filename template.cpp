323ce18d66073070e92cbdf77e0833b3b212a51c
Michael <mvd1409@gmail.com>
2015-10-26 19:57:08 -0400

    Removed code to fix the template

--- template.cpp
+++ template.cpp
@@ -3,13 +3,6 @@ using namespace std;
 
 int main() {
 
-	float fahrenheit, celsius;
-
-	cout << "Enter the temperature in Celsius : ";
-	cin >> celsius;
-	fahrenheit = (celsius * 9.0) / 5.0 + 32;
-	cout << "The temperature in Celsius    : " << celsius << endl;
-	cout << "The temperature in Fahrenheit : " << fahrenheit << endl;
 
 	// SPACER ---------------------------------------------------------
 	printf("\n");
