a42346090ab07e26670204d112ddc5d496feb584
Michael <mvd1409@gmail.com>
2015-10-26 20:55:32 -0400

    Create ascii_drawing_snippet.cpp

---
+++ ASCII-Drawing/ascii_drawing_snippet.cpp
@@ -0,0 +1,8 @@
+	cout << "     A      R1        B         C" << endl;
+	cout << "     .____/\\/\\/\\______._________." << endl;
+	cout << "     |                |         |" << endl;
+	cout << "    ___               >         <" << endl;
+	cout << " Vs  -                > R2      < R3" << endl;
+	cout << "     |                >         <" << endl;
+	cout << "     |                |         |" << endl;
+	cout << "     |________________|_________|" << endl;
