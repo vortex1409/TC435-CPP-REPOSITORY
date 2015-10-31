738ba1ef89c576c6c20b5a4cfbf286c065255334
Michael <mvd1409@gmail.com>
2015-10-31 14:09:21 -0400

    Create source.cpp

---
+++ Final-Project/source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	// SPACER ---------------------------------------------------------
+	cout << endl;
+	cout << "________________________________________________" << endl;
+	system("pause");
+	return 0;
+}
