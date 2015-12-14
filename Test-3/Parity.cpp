3c9af578b5d26e5b7312a7d33055213a942fa513
Michael <mvd1409@gmail.com>
2015-12-14 09:55:14 -0500

    Create Parity.cpp

---
+++ Test-3/Parity.cpp
@@ -0,0 +1,16 @@
+		if (parity == 1 && X == 1)
+		{
+			bit = 0;
+		}
+		else if (parity == 1 && X == 0)
+		{
+			bit = 1;
+		}
+		else if (parity == 0 && X == 1)
+		{
+			bit = 1;
+		}
+		else if (parity == 0 && X == 0)
+		{
+			bit = 0;
+		}
