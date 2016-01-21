0e3bb7c77e9624f99663c13816f6f58935a92221
Michael <mvd1409@gmail.com>
2016-01-21 14:27:04 -0500

    Create mdorfman_template.cpp

---
+++ mdorfman_template.cpp
@@ -0,0 +1,22 @@
+#include <iostream> // Core
+#include <cmath> // Added to remove 'ambiguous'
+using namespace std;
+
+//--------- FUNCTIONS START -------------------
+
+
+
+//--------- FUNCTIONS END --------------------
+
+int main() {
+
+
+
+	//--------- MAIN END --------------------
+	cout << endl;
+	system("pause");
+	return 0;
+}
+//TC435
+// 2015 (C) MICHAEL DORFMAN | VORTEX1409 | base64(TUlDSEFFTCBET1JGTUFOIHwgVk9SVEVYMTQwOQ==)
+//MTQwOQ==
