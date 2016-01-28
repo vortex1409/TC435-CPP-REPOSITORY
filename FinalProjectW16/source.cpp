f1bde3693e3cdd74c04455088cbab3f233d0aec4
Michael <mvd1409@gmail.com>
2016-01-28 17:02:16 -0500

    Update source.cpp

--- FinalProjectW16/source.cpp
+++ FinalProjectW16/source.cpp
@@ -1,1 +1,23 @@
-jgagf
+#include <iostream> // Core
+#include <cmath> // Added to remove 'ambiguous'
+#include "name_w16.h"
+using namespace std;
+
+//--------- FUNCTIONS START -------------------
+
+
+
+//--------- FUNCTIONS END --------------------
+
+int main() {
+
+
+
+	//--------- MAIN END --------------------
+	cout << endl;
+	system("pause");
+	return 0;
+}
+//TC435
+// 2015 (C) MICHAEL DORFMAN | VORTEX1409 | base64(TUlDSEFFTCBET1JGTUFOIHwgVk9SVEVYMTQwOQ==)
+//MTQwOQ==
