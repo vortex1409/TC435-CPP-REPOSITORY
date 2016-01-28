b929b40da8217ddae80641597ec1ad9c472edcd9
Michael <mvd1409@gmail.com>
2016-01-28 17:00:52 -0500

    Update firstTest.cpp

--- FinalProjectW16/firstTest.cpp
+++ FinalProjectW16/firstTest.cpp
@@ -1,1 +1,10 @@
 //Put this file in Resource Files
+#include <iostream>
+#include "name_w16.h"
+using namespace std;
+
+//Define Functions
+void firstTest(int input)
+{
+  cout << "Test #: " << input << endl;
+}
