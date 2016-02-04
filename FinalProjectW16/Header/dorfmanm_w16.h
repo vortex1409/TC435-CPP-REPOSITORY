4f2d3fb0e1183bfae3c32cd5bb4264ed80ebaff4
Michael <mvd1409@gmail.com>
2016-02-04 15:34:37 -0500

    Update dorfmanm_w16.h

--- FinalProjectW16/Header/dorfmanm_w16.h
+++ FinalProjectW16/Header/dorfmanm_w16.h
@@ -1,10 +1,11 @@
 #include <iostream>
 
-//Declare Functions
-//Only Declare Not Define
-
 void firstTest(int);
 
 void quiz_1(double);
 
 void quiz_2(int, char);
+
+int AmodB(int, int);
+
+double customEquation_1(double, double, double);
