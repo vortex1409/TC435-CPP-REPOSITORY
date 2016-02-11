e1a1483b929cd938fca96501bbd75e40dffd336f
Michael <mvd1409@gmail.com>
2016-02-11 17:16:47 -0500

    Update main.cpp

--- FPW16/main.cpp
+++ FPW16/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // Core
 #include <cmath> // Added to remove 'ambiguous'
-#include "dorfmanm_w16.h"
+#include "dorfmanm_w16.h" // Stored in Same DIR Relative to main.cpp
 using namespace std;
 
 //--------- FUNCTIONS START -------------------
@@ -19,13 +19,32 @@ int main() {
 	cout << "Custom Equation 1" << endl;
 	cout << customEquation_1(1, 1, 1) << endl;
 
-		
+	int miscArray[] = { 1,2,3,4,5,-1,-3 };
+
+	PLC controller_1;
+	controller_1.PV = 30;
+
+	customEquation_2(miscArray, 4);
+
+	//------CLASSES START--------------
+
+	firstClass testClass;
+	testClass.firstfunction();
+
+	cout << endl;
+	cout << testClass.addTwo(3, 4) << endl;
+
+	cout << testClass.misc << endl;
+
+
 
 	//--------- MAIN END --------------------
 	cout << endl;
 	system("pause");
 	return 0;
 }
+
+
 //TC435
 // 2015 (C) MICHAEL DORFMAN | VORTEX1409 | base64(TUlDSEFFTCBET1JGTUFOIHwgVk9SVEVYMTQwOQ==)
 //MTQwOQ==
