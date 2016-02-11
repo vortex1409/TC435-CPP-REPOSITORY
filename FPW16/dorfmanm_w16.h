f054787dbcb0bdeb4aa2d3812bfd820adcabe976
Michael <mvd1409@gmail.com>
2016-02-10 22:29:52 -0500

    Create dorfmanm_w16.h

---
+++ FPW16/dorfmanm_w16.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//--------------- DECLARING FUNCTIONS ----------------------
+
+void firstTest(int);
+
+void quiz_1(double);
+
+void quiz_2(int, char);
+
+int AmodB(int, int);
+
+double customEquation_1(double, double, double);
+
+//------------- DECLARING STRUCTURES --------------------------
+
+struct transistor
+{
+	double hfe;
+	double vce, ic, ie, ib;
+	char npnorpnp;
+};
