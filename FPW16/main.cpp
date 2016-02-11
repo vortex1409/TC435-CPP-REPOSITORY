dafd43d7a1061668d5db5b0f2bc1538de0b0ac8f
Michael <mvd1409@gmail.com>
2016-02-10 22:29:05 -0500

    Create main.cpp

---
+++ FPW16/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream> // Core
+#include <cmath> // Added to remove 'ambiguous'
+#include "dorfmanm_w16.h"
+using namespace std;
+
+//--------- FUNCTIONS START -------------------
+
+
+
+//--------- FUNCTIONS END --------------------
+
+int main() {
+
+	firstTest(1);
+
+	cout << "AmoD Function" << endl;
+	cout << AmodB(3, 2) << endl;
+
+	cout << "Custom Equation 1" << endl;
+	cout << customEquation_1(1, 1, 1) << endl;
+
+		
+
+	//--------- MAIN END --------------------
+	cout << endl;
+	system("pause");
+	return 0;
+}
+//TC435
+// 2015 (C) MICHAEL DORFMAN | VORTEX1409 | base64(TUlDSEFFTCBET1JGTUFOIHwgVk9SVEVYMTQwOQ==)
+//MTQwOQ==
