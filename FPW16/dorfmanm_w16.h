e287eebfc5b5df1ad0e39c38695545bd71ffecfd
Michael <mvd1409@gmail.com>
2016-02-11 17:16:59 -0500

    Update dorfmanm_w16.h

--- FPW16/dorfmanm_w16.h
+++ FPW16/dorfmanm_w16.h
@@ -1,5 +1,12 @@
+//#pragma once
+#ifndef DORFMAN_W16_H
+#define DORFMAN_W16_H
 #include <iostream>
-
+//Name : Michael Dorfman
+// COurce: TC435
+// Project: Custom Header
+// Header for keeping functions and other code together to be used
+//in any program
 //--------------- DECLARING FUNCTIONS ----------------------
 
 void firstTest(int);
@@ -12,6 +19,10 @@ int AmodB(int, int);
 
 double customEquation_1(double, double, double);
 
+void customEquation_2(int [], int);
+
+void intToHex(int);
+
 //------------- DECLARING STRUCTURES --------------------------
 
 struct transistor
@@ -20,3 +31,26 @@ struct transistor
 	double vce, ic, ie, ib;
 	char npnorpnp;
 };
+
+struct PLC
+{
+	double SP, PV, ProportionalG, DifferentialG, IntegralG, Alarm_1, Alarm_2, Output;
+};
+
+// ------------- DECLARING CLASSES-----------------------
+class firstClass
+{
+public: // Info Gathered
+
+	void firstfunction();
+	int addTwo(int, int);
+	int misc = 1;
+
+
+private: // Info
+
+	int num1 = 1;
+	int num2 = 2;
+};
+
+#endif
