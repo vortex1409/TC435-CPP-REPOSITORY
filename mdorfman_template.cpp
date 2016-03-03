93af8e6b610e33a2cbccedfe16007112a99f23f3
Michael <mvd1409@gmail.com>
2016-03-03 15:33:11 -0500

    Update mdorfman_template.cpp

--- mdorfman_template.cpp
+++ mdorfman_template.cpp
@@ -1,22 +1,22 @@
 #include <iostream> // Core
 #include <cmath> // Added to remove 'ambiguous'
+#include <cstring>
+#include <fstream>
+#include "dorfmanm_w16.h" // Stored in Same DIR Relative to main.cpp
 using namespace std;
 
-//--------- FUNCTIONS START -------------------
-
-
-
-//--------- FUNCTIONS END --------------------
-
 int main() {
+	//--------- MAIN START ------------------
 
+	
 
-
-	//--------- MAIN END --------------------
+	// -------- MAIN END --------------------
 	cout << endl;
 	system("pause");
 	return 0;
 }
+
+
 //TC435
 // 2015 (C) MICHAEL DORFMAN | VORTEX1409 | base64(TUlDSEFFTCBET1JGTUFOIHwgVk9SVEVYMTQwOQ==)
 //MTQwOQ==
