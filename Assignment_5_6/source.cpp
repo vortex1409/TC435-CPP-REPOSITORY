e3d5db8175cdf91f77e66be38eb50df6de74e785
Michael <mvd1409@gmail.com>
2016-03-15 14:19:35 -0400

    Create source.cpp

---
+++ Assignment_5_6/source.cpp
@@ -0,0 +1,24 @@
+double pie();
+double exponent(double);
+
+
+double pie
+{
+  double pi = 3.14159265;
+  return pi;
+}
+
+double exponent(double input)
+{
+  double e = 2.718281828;
+}
+
+
+
+struct PLC
+{
+	double SP, PV;
+	double ProportionalG, DifferentialG, IntegralG;
+	double System_T, System_G;
+	double CharacteristicEffects[2];
+};
