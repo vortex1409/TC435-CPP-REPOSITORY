150d03025fb58464abaeb4358fcd9fe5727a6db3
Michael <mvd1409@gmail.com>
2016-04-08 10:01:02 -0400

    Update main.cpp

--- FPW16/main.cpp
+++ FPW16/main.cpp
@@ -8,20 +8,47 @@ using namespace std;
 int main() {
 	//--------- MAIN START ------------------
 
-	plotter bandPass;
-	filter_RC filterToTest;
+	//plotter bandPass;
+	//filter_RC filterToTest;
 
-	initializeFilter(filterToTest);
+	//initializeFilter(filterToTest);
 
-	bandPass.axis_MIN = 1;
-	bandPass.axis_MAX = 500000;
-	bandPass._step = 10;
+	//bandPass.axis_MIN = 1;
+	//bandPass.axis_MAX = 500000;
+	//bandPass._step = 10;
 
-	plotFilter(bandPass, filterToTest);
+	//plotFilter(bandPass, filterToTest);
 
-	cout << filterToTest.Fr << endl;
-	cout << filterToTest.Fh << endl;
-	cout << filterToTest.Fl << endl;
+	//cout << filterToTest.Fr << endl;
+	//cout << filterToTest.Fh << endl;
+	//cout << filterToTest.Fl << endl;
+
+	uController tester;
+
+	tester.print("w");
+	cout << endl;
+
+	tester.initializeController();
+
+	tester.print("W");
+	cout << endl;
+
+	tester.print("w");
+	cout << endl;
+
+	tester.MOVLW("31");
+	cout << endl;
+
+	tester.print("w");
+	cout << endl;
+
+	tester.print("W");
+	cout << endl;
+
+	tester.stopController();
+
+	tester.print("w");
+	cout << endl;
 
 	// -------- MAIN END --------------------
 	cout << endl;
